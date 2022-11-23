33b02c2151441e6ce98d8c79535f14ea989b121f
BabyDragon003 <golddragon01234@gmail.com>
2022-11-23 18:54:52 +0900

    "2022-11-23 18:54"

--- contracts/terrapexc_pair/src/error.rs
+++ contracts/terrapexc_pair/src/error.rs
@@ -3,6 +3,12 @@ use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+
+    #[error("{0}")]
+    OverflowError(#[from] OverflowError),
+
     #[error("Unauthorized")]
     Unauthorized {},
 
