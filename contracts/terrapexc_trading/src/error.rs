8d3f0363f84025eee5c8fc669c53573fb483505f
BabyDragon003 <golddragon01234@gmail.com>
2022-11-30 18:54:52 +0900

    "2022-11-30 18:54"

--- contracts/terrapexc_trading/src/error.rs
+++ contracts/terrapexc_trading/src/error.rs
@@ -8,17 +8,6 @@ pub enum ContractError {
 
     #[error("{0}")]
     OverflowError(#[from] OverflowError),
-
-    #[error("Unauthorized")]
-    Unauthorized {},
-
-    #[error("Not Reward or Order token")]
-    UnacceptableToken {},
-
-    #[error("Invalid zero amount")]
-    InvalidZeroAmount {},
-
-    #[error("InvalidInput")]
     InvalidInput {},
 
     #[error("Not enough Reward")]
